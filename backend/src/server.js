235199559d30f55f32859f197f56f502f91cbb5d
mamitakaAnao <swrsmrtyn0@gmail.com>
2024-12-24 14:02:23 +0200

    moved into rest api and added env variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+require('dotenv').config()
+const PORT = process.env.PORT; //env variables, requires .env file
+
+const apiRouter = require('./routes/api');
 
 const cors = require('cors'); // Import the cors package
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: 'http://localhost:' + PORT, // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true // If you need to send cookies or authentication headers
 }));
 
-// Root route to confirm server is running
-app.get('/', (req, res) => {
-  res.send('Express server is running.');
-});
-
 // middelware to parse json bodies
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+// Root route to confirm server is running
+app.get('/', (req, res) => {
+  res.status(200).json({connection: true});
 });
 
-app.post('/api/upload-excel-names', (req, res) => {
-  const jsonData = req.body
-  console.log("recived json data", jsonData)
-  res.status(200).json({ message: 'Excel data received successfully.' });
+app.use('/api', apiRouter);
 
-})
 app.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}`);
 });
