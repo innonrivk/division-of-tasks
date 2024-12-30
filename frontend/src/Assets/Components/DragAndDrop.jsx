a17613b1eba4de4da1145b712565b827a624786f
Innon Rivkin <innonrivk@gmail.com>
2024-12-30 15:58:14 +0200

    upload file screen file drop and upload btn are done

--- frontend/src/Assets/Components/DragAndDrop.jsx
+++ frontend/src/Assets/Components/DragAndDrop.jsx
@@ -1,34 +1,47 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { FileUploader } from "react-drag-drop-files";
 import { useDropzone } from 'react-dropzone'
 import './DragAndDrop.css'
 const fileTypes = ["xls", "xlsx"];
 
 
-function DragAndDrop() {
+function DragAndDrop(props) {
+  const [file, setFile] = useState("")
+
+useEffect(() => {
+    setFile("")
+
+}, [props.reset]) 
+
 
   const onDrop = ((acceptedFiles) => {
     const file = acceptedFiles[0]
-    try{
-    if(file){
+    setFile(file["name"])
+    try {
+      if (file) {
         console.log("file", file)
+      }
     }
-}
-catch(err){
-    console.log(err)
-}
+    catch (err) {
+      console.log(err)
+    }
 
   })
 
+  const clickHandler = () => {
+    console.log("click")
+    setFile("")
+  }
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
- 
+
   return (
     <div {...getRootProps({
       className: `dropzone ${isDragActive ? "dropzone-active" : ""}`,
     })}>
       <input {...getInputProps()} />
-        <p>גרור קובץ לכאן</p>
+      {file != "" ?  <p>{file}</p>:<p>גרור קובץ לכאן</p>}
       <button>חפש</button>
 
     </div>
--- frontend/src/Screens/NameFileUploadScreen.jsx
+++ frontend/src/Screens/NameFileUploadScreen.jsx
@@ -1,18 +1,22 @@
-import React ,{ useState} from 'react'
+import React, { useState } from 'react'
 import FileBrowser from '../Assets/Components/FileBrowser'
 import DragAndDrop from '../Assets/Components/DragAndDrop'
 import "./NameFileUploadScreen.css"
 function NameFileUploadScreen() {
+  const [reset, setReset] = useState(false)
+  const uploadHandler = () => {
+    setReset(prev => !prev)
+  }
 
   return (
     <div className='container'>
-     
+
       <div className='container-component-drag-and-drop'>
-      <DragAndDrop></DragAndDrop>
+        <DragAndDrop reset={reset}></DragAndDrop>
       </div>
       <div className='container-component-upload-btn '>
         <div className='component-upload-btn'>
-          <button>
+          <button onClick={uploadHandler}>
             העלאה
           </button>
         </div>
